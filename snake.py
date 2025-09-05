b97d493ba310ebc7fee3f884c923ca3ce1453b07
betonomehalka <nise123david@gmail.com>
2025-09-05 20:11:31 +0500

    snake.py
    v0.1
    Просто добавлено управление

--- snake.py
+++ snake.py
@@ -22,20 +22,53 @@ class GameSprite(sprite.Sprite):
     def draw_sprite(self):
         w.blit(self.image, (self.rect.x, self.rect.y))
 
-head = GameSprite('head.png', 200, 250)
+class Snake(GameSprite):
+    def __init__(self, pl_image, pl_x, pl_y):
+        super().__init__(pl_image, pl_x, pl_y)
+        self.dx = SQUARE_SIZE
+        self.dy = 0
+    
+    def update(self):
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+    
+    def get_diction(self):
+        keys = key.get_pressed()
+        if keys [K_UP] and self.dy == 0:
+            self.dx = 0
+            self.dy = -SQUARE_SIZE
+        if keys [K_DOWN] and self.dy == 0:
+            self.dx = 0
+            self.dy = SQUARE_SIZE
+        if keys [K_LEFT] and self.dx == 0:
+            self.dx = -SQUARE_SIZE
+            self.dy = 0
+        if keys [K_RIGHT] and self.dx == 0:
+            self.dx = SQUARE_SIZE
+            self.dy = 0
+
+
+head = Snake('head.png', 200, 250)
 clock = time.Clock()
-
+step_time = timer() 
 running = True
-
 finish = False
+
 while running:
     for e in event.get():
         if e.type == QUIT:
             running = False
     
     if not finish:
+        cur_time = timer()
+        head.get_diction()
         w.blit(bg, (0, 0))
         head.draw_sprite()
+
+    if cur_time - step_time >= 0.5:
+        head.update()
+        step_time = timer()
+    head.draw_sprite()
     
     display.update()
     clock.tick(FPS)
